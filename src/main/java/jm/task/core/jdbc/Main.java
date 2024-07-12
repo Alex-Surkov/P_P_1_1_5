f5f575c2a7bd2371220453c8be84eeab2977105f
Alex-Surkov <127119419+Alex-Surkov@users.noreply.github.com>
2024-07-12 16:07:40 +0300

    main class added

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,6 +1,16 @@
 package jm.task.core.jdbc;
+import jm.task.core.jdbc.service.UserServiceImpl;
 
 public class Main {
     public static void main(String[] args) {
+        UserServiceImpl userServiceImpl = new UserServiceImpl();
+        userServiceImpl.createUsersTable();
+        userServiceImpl.saveUser("Василий", "Иванов", (byte) 5);
+        userServiceImpl.saveUser("Гасилий", "Иванов", (byte) 4);
+        userServiceImpl.saveUser("Басилий", "Иванов", (byte) 3);
+        userServiceImpl.saveUser("Сасилий", "Иванов", (byte) 2);
+        System.out.println(userServiceImpl.getAllUsers());
+        userServiceImpl.cleanUsersTable();
+        userServiceImpl.dropUsersTable();
     }
 }
